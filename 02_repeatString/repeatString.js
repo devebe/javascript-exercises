bbd6cb1fc8468f135e233747bb7ca7e74aec163b
devebe <dvbirdja@gmail.com>
2022-08-10 22:34:57 +0200

    Solve exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+let string;
+let num = 0;
+const repeatString = function(string, num) {
+    parseInt(num);
+    let result = ''
+    if (num < 0) {
+        result = 'ERROR';
+    }
+    else if (num === 0) {
+        result = '';
+    }
+    else {
+        for (let i = 0; i < num; i++) {
+            result = result + string;
+        }
+    }
+    return result;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
