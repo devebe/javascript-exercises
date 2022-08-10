ef36290d92cf239ba7ef28aad0891df4f36b5956
devebe <dvbirdja@gmail.com>
2022-08-10 22:47:49 +0200

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const forwardArray = string.split("");
+    let reverseText = '';
+    for (let i = 0; i < forwardArray.length; i++) {
+        reverseText += forwardArray[forwardArray.length - i - 1];
+    }
+    return reverseText;
 };
 
 // Do not edit below this line
