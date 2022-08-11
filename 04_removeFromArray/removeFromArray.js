46843c02624e0f68981ff4e44a162399fd6990dd
devebe <dvbirdja@gmail.com>
2022-08-11 22:29:32 +0200

    Solve exercise 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(input,number) {
+    const arrReturn = new Array();
+    for (let i = 0; i < input.length; i++) {
+        
+        if (number != input[i]) {
+            arrReturn.push(input[i]);
+        }
+    }
+    return arrReturn;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
