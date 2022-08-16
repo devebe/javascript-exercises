cc06bd5279f757d360a3e053bc2348e072f5b46f
devebe <dvbirdja@gmail.com>
2022-08-16 18:25:18 +0200

    adjust function for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    let startValue = start - 1;
+    let endValue = end;
+    let result = 0.5 * ((endValue * (endValue + 1)) - (startValue * (startValue + 1)));
+    return result;
 };
 
 // Do not edit below this line
