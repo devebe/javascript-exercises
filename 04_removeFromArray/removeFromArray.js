3a7a55c15e991322755d7ffbb2a3be70320ecaec
devebe <dvbirdja@gmail.com>
2022-08-16 17:57:58 +0200

    remove all failing code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,6 @@
-const removeFromArray = function(input,numbersToRemove) {
-    const arrReturn = new Array();
-    for (let i = 0; i < numbersToRemove.length; i++) {
-
-        let num = numbersToRemove[i];
-
-        for (let j = 0; j < input.length; j++) {
-
-            if (num != input[j]) {
-                arrReturn.push(input[i]);
-            }
-        }
-    }
-    return arrReturn;
+const removeFromArray = function(input,...number) {
+    
 }
+
 // Do not edit below this line
 module.exports = removeFromArray;
