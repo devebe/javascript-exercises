eda423b608135a99fcebd40d2b92d2ff66a3962b
devebe <dvbirdja@gmail.com>
2022-08-16 20:56:31 +0200

    create functions for temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(conversionValue) {
+  let celciusValue = (conversionValue - 32) * (5/9);
+  return parseFloat(celciusValue.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(conversionValue) {
+  let fahrenheitValue = conversionValue * (9/5) + 32;
+  return parseFloat(fahrenheitValue.toFixed(1));  
 };
 
 // Do not edit below this line
