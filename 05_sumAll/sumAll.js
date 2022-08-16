34521a69b9e06d49e8224397842b8cfa992b039d
devebe <dvbirdja@gmail.com>
2022-08-16 18:29:59 +0200

    add condition to start with smallest of 2 numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
 const sumAll = function(start,end) {
-    let startValue = start - 1;
-    let endValue = end;
+    let startValue;
+    let endValue;
+    if (start > end) {
+        startValue = end - 1;
+        endValue = start;
+    }
+    else {
+        startValue = start - 1;
+        endValue = end;
+    }    
     let result = 0.5 * ((endValue * (endValue + 1)) - (startValue * (startValue + 1)));
     return result;
 };
