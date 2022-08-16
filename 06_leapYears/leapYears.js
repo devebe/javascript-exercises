8fb49c5156c50aff96340d63687722e06ff90aad
devebe <dvbirdja@gmail.com>
2022-08-16 20:33:24 +0200

    Create conditional statements for testing leapYear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    // start with the highest moduli
+    let isLeapYear;
+    if (inputYear % 400 == 0){
+        isLeapYear = true;
+    }
+    else if (inputYear % 100 != 0 && inputYear % 4 == 0){
+        isLeapYear = true;
+    }
+    else {
+        isLeapYear = false;
+    }
+    return isLeapYear;
 };
 
 // Do not edit below this line
