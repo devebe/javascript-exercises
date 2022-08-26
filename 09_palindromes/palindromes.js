f0826db7c169cef7887427908c2e01d4495f45d6
devebe <dvbirdja@gmail.com>
2022-08-26 17:34:45 +0200

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.replace(/[^A-z]/g, '').toLowerCase().split('');
+    for (let i = 0; i < newString.length; i++) {
+        if (newString[i] !== newString[(newString.length - 1) - i] ){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
